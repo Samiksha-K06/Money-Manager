b0b5f2b7f8e715779a435553e967a649527ddb71
Samiksha-K06 <samiksha.k0610@gmail.com>
2026-07-05 18:43:01 +0530

    test

--- src/main/java/com/samiksha/moneymanager/service/EmailService.java
+++ src/main/java/com/samiksha/moneymanager/service/EmailService.java
@@ -13,7 +13,7 @@ public class EmailService {
 	
 	private final JavaMailSender mailSender;
 	
-	@Value("${spring.mail.properties.smtp.from}")
+	@Value("${spring.mail.from}")
 	private String fromEmail;
 	
 	public void sendEmail(String to, String subject, String body) {
